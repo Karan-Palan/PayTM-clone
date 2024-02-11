43cd4cfc2315222a6be4fa9af81b6d7b22399281
Karan-Palan <karanpalan007@gmail.com>
2024-02-11 13:38:41 +0530

    Created Database

--- backend/db.js
+++ backend/db.js
@@ -1,2 +1,16 @@
 const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://karanpalan007:7pLEVZi924aPpdIE@cluster0.q977rka.mongodb.net/?retryWrites=true&w=majority"
+);
 
+const userSchema = mongoose.Schema({
+  username: String,
+  password: String,
+  fName: String,
+  lName: String,
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports({
+  User,
+});
